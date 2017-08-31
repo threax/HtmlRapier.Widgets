78d8e76cfa5f8caf3eaed75a6cec26f440ecf2f0
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-31 10:33:01 -0400

    Moved CrudTableController to its own file.

--- src/CrudPage.ts
+++ src/CrudPage.ts
@@ -22,74 +22,8 @@ import { CrudItemEditorController, CrudItemEditorControllerExtensions, CrudItemE
 export { CrudItemEditorController, CrudItemEditorControllerExtensions, CrudItemEditorControllerOptions, CrudItemEditorType } from 'hr.widgets.CrudItemEditor';
 import { CrudPageNumbers } from 'hr.widgets.CrudPageNumbers';
 export { CrudPageNumbers } from 'hr.widgets.CrudPageNumbers';
-
-export class CrudTableController extends ListingDisplayController<any> {
-    public static get InjectorArgs(): controller.InjectableArgs {
-        return [controller.BindingCollection, ListingDisplayOptions, ICrudService, CrudQueryManager, controller.InjectedControllerBuilder];
-    }
-
-    private crudService: ICrudService;
-    private queryManager: CrudQueryManager;
-    private addToggle: controller.OnOffToggle;
-    private lookupDisplaySchema = true;
-
-    constructor(bindings: controller.BindingCollection, options: ListingDisplayOptions, crudService: ICrudService, queryManager: CrudQueryManager, private builder: controller.InjectedControllerBuilder) {
-        super(bindings, options);
-
-        this.queryManager = queryManager;
-        this.crudService = crudService;
-        this.crudService.dataLoadingEvent.add(a => this.handlePageLoad(a.data));
-        this.addToggle = bindings.getToggle("add");
-        if (options.setLoadingOnStart) {
-            this.crudService.getPage(queryManager.setupQuery()); //Fires async
-        }
-    }
-
-    public add(evt: Event) {
-        evt.preventDefault();
-        this.crudService.add();
-    }
-
-    public setData(pageData: any) {
-        var items = this.crudService.getItems(pageData);
-        this.clearData();
-        var listingCreator = this.builder.createOnCallback(CrudTableRowController);
-        for (var i = 0; i < items.length; ++i) {
-            var itemData = this.crudService.getListingDisplayObject(items[i]);
-            this.appendData(itemData, (b, d) => {                
-                listingCreator(b, items[i]);
-            });
-        }
-        this.showMain();
-    }
-
-    private async handlePageLoad(promise: Promise<any>) {
-        this.showLoad();
-
-        try {
-            var data = await promise; //Important to await this first
-
-            if (this.lookupDisplaySchema) {
-                this.lookupDisplaySchema = false;
-                var schema = await this.crudService.getListingSchema();
-                if (schema) {
-                    this.setFormatter(new view.SchemaViewDataFormatter(schema));
-                }
-            }
-
-            this.setData(data);
-
-            if (this.addToggle && ! await this.crudService.canAdd()) {
-                this.addToggle.off();
-            }
-            this.addToggle = undefined; //Saw this once, thats all we care about
-        }
-        catch (err) {
-            console.log("Error loading crud table data. Message: " + err.message);
-            this.showError(err);
-        }
-    }
-}
+import { CrudTableController } from 'hr.widgets.CrudTableController';
+export { CrudTableController } from 'hr.widgets.CrudTableController';
 
 /**
  * Setup the services to use a crud page in the given service collection. This will
---
+++ src/CrudTableController.ts
@@ -0,0 +1,74 @@
+import { ListingDisplayController, ListingDisplayOptions } from 'hr.widgets.ListingDisplayController';
+import * as controller from 'hr.controller';
+import { DataLoadingEventArgs, ICrudService, ItemEditorClosedCallback, ItemUpdatedCallback, ShowItemEditorEventArgs } from 'hr.widgets.CrudService';
+import { CrudQueryManager, ICrudQueryComponent, QueryEventArgs } from 'hr.widgets.CrudQuery';
+import { CrudTableRowController, CrudTableRowControllerExtensions, addServices as addRowServices } from 'hr.widgets.CrudTableRow';
+import * as view from 'hr.view';
+
+export class CrudTableController extends ListingDisplayController<any> {
+    public static get InjectorArgs(): controller.InjectableArgs {
+        return [controller.BindingCollection, ListingDisplayOptions, ICrudService, CrudQueryManager, controller.InjectedControllerBuilder];
+    }
+
+    private crudService: ICrudService;
+    private queryManager: CrudQueryManager;
+    private addToggle: controller.OnOffToggle;
+    private lookupDisplaySchema = true;
+
+    constructor(bindings: controller.BindingCollection, options: ListingDisplayOptions, crudService: ICrudService, queryManager: CrudQueryManager, private builder: controller.InjectedControllerBuilder) {
+        super(bindings, options);
+
+        this.queryManager = queryManager;
+        this.crudService = crudService;
+        this.crudService.dataLoadingEvent.add(a => this.handlePageLoad(a.data));
+        this.addToggle = bindings.getToggle("add");
+        if (options.setLoadingOnStart) {
+            this.crudService.getPage(queryManager.setupQuery()); //Fires async
+        }
+    }
+
+    public add(evt: Event) {
+        evt.preventDefault();
+        this.crudService.add();
+    }
+
+    public setData(pageData: any) {
+        var items = this.crudService.getItems(pageData);
+        this.clearData();
+        var listingCreator = this.builder.createOnCallback(CrudTableRowController);
+        for (var i = 0; i < items.length; ++i) {
+            var itemData = this.crudService.getListingDisplayObject(items[i]);
+            this.appendData(itemData, (b, d) => {                
+                listingCreator(b, items[i]);
+            });
+        }
+        this.showMain();
+    }
+
+    private async handlePageLoad(promise: Promise<any>) {
+        this.showLoad();
+
+        try {
+            var data = await promise; //Important to await this first
+
+            if (this.lookupDisplaySchema) {
+                this.lookupDisplaySchema = false;
+                var schema = await this.crudService.getListingSchema();
+                if (schema) {
+                    this.setFormatter(new view.SchemaViewDataFormatter(schema));
+                }
+            }
+
+            this.setData(data);
+
+            if (this.addToggle && ! await this.crudService.canAdd()) {
+                this.addToggle.off();
+            }
+            this.addToggle = undefined; //Saw this once, thats all we care about
+        }
+        catch (err) {
+            console.log("Error loading crud table data. Message: " + err.message);
+            this.showError(err);
+        }
+    }
+}
