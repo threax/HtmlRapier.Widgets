157ed31e967282d3b967657e767d7ec5a082c31b
Andrew Piper <piper.andrew@spcollege.edu>
2017-09-29 13:33:33 -0400

    Added a load, main, error lifecycle to search to show loading while the schema is loading.

--- src/CrudSearch.ts
+++ src/CrudSearch.ts
@@ -2,6 +2,7 @@ import { CrudQueryManager, ICrudQueryComponent, QueryEventArgs } from 'hr.widget
 import * as controller from 'hr.controller';
 import { ICrudService } from 'hr.widgets.CrudService';
 import * as form from 'hr.form';
+import { MainLoadErrorLifecycle } from 'hr.widgets.MainLoadErrorLifecycle';
 
 export class CrudSearch extends ICrudQueryComponent {
     public static get InjectorArgs(): controller.InjectableArgs {
@@ -11,9 +12,17 @@ export class CrudSearch extends ICrudQueryComponent {
     private queryManager: CrudQueryManager;
     private crudService: ICrudService;
     private form: controller.IForm<any>;
+    private lifecycle: MainLoadErrorLifecycle;
 
     constructor(bindings: controller.BindingCollection, crudService: ICrudService, queryManager: CrudQueryManager) {
         super();
+
+        this.lifecycle = new MainLoadErrorLifecycle(
+            bindings.getToggle("main"),
+            bindings.getToggle("load"),
+            bindings.getToggle("error"),
+            true);
+
         this.crudService = crudService;
         this.crudService.dataLoadingEvent.add(a => this.handlePageLoad(a.data));
         this.queryManager = queryManager;
@@ -23,19 +32,26 @@ export class CrudSearch extends ICrudQueryComponent {
     }
 
     private async setup(bindings: controller.BindingCollection, crudService: ICrudService) {
-        var schema: any = await crudService.getSearchSchema();
-        //Look for x-ui-search properties that are true
-        var properties = schema.properties;
-        if (properties) {
-            for (var key in properties) {
-                var prop = properties[key];
-                if (prop["x-ui-search"] !== true) {
-                    delete properties[key]; //Delete all properties that do not have x-ui-search set.
+        try {
+            this.lifecycle.showLoad();
+            var schema: any = await crudService.getSearchSchema();
+            //Look for x-ui-search properties that are true
+            var properties = schema.properties;
+            if (properties) {
+                for (var key in properties) {
+                    var prop = properties[key];
+                    if (prop["x-ui-search"] !== true) {
+                        delete properties[key]; //Delete all properties that do not have x-ui-search set.
+                    }
                 }
             }
-        }
 
-        this.form.setSchema(schema);
+            this.form.setSchema(schema);
+            this.lifecycle.showMain();
+        }
+        catch (err) {
+            this.lifecycle.showError(err);
+        }
     }
 
     public setupQuery(query: any): void {
