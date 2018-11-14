8aff44cfbe5ad16e14765a13eabe69cebad7f51b
Andrew Piper <piper.andrew@spcollege.edu>
2018-11-14 09:47:47 -0500

    Added toggles to display order.

--- src/CrudSort.ts
+++ src/CrudSort.ts
@@ -1,6 +1,15 @@
 import { CrudQueryManager, ICrudQueryComponent, QueryEventArgs } from 'hr.widgets.CrudQuery';
 import * as controller from 'hr.controller';
 import { ICrudService } from 'hr.widgets.CrudService';
+import * as toggles from 'hr.toggles';
+
+class ColumnOrderToggle extends toggles.TypedToggle {
+    public getPossibleStates(): string[] {
+        return ['asc', 'desc', 'off'];
+    }
+}
+
+type ColumnToggleMap = { [key: string]: ColumnOrderToggle };
 
 export class CrudSort extends ICrudQueryComponent {
     public static get InjectorArgs(): controller.InjectableArgs {
@@ -9,11 +18,14 @@ export class CrudSort extends ICrudQueryComponent {
 
     private orderBy: string;
     private orderAsc: boolean = true;
+    private orderToggles: toggles.Group;
+    private loadedToggles: ColumnToggleMap = {};
 
-    constructor(bindings: controller.BindingCollection, private crudService: ICrudService, private queryManager: CrudQueryManager) {
+    constructor(private bindings: controller.BindingCollection, private crudService: ICrudService, private queryManager: CrudQueryManager) {
         super();
         this.crudService.dataLoadingEvent.add(a => this.handlePageLoad(a.data));
         this.queryManager.addComponent(this);
+        this.orderToggles = new toggles.Group();
     }
 
     public setupQuery(query: any): void {
@@ -30,6 +42,7 @@ export class CrudSort extends ICrudQueryComponent {
         var strongData = this.crudService.getSearchObject(pageData);
         this.orderBy = strongData.orderBy;
         this.orderAsc = strongData.order !== "Descending";
+        this.handleSortIcons();
     }
 
     public sort(evt: Event): void {
@@ -44,6 +57,25 @@ export class CrudSort extends ICrudQueryComponent {
         this.orderBy = newSortColumn;
 
         this.crudService.getPage(this.queryManager.setupQuery());
+        this.handleSortIcons();
+    }
+
+    private handleSortIcons() {
+        var toggleName = this.orderBy + "OrderToggle";
+        var columnToggle: ColumnOrderToggle;
+        if (this.loadedToggles[toggleName] === undefined) {
+            columnToggle = this.bindings.getCustomToggle(toggleName, new ColumnOrderToggle());
+            this.loadedToggles[toggleName] = columnToggle;
+            this.orderToggles.add(columnToggle);
+        }
+        else {
+            columnToggle = this.loadedToggles[toggleName];
+        }
+        var orderName = "desc";
+        if (this.orderAsc) {
+            orderName = "asc";
+        }
+        this.orderToggles.activate(columnToggle, orderName, "off");
     }
 
     private async handlePageLoad(promise: Promise<any>) {
