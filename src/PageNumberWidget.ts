46927bad2d5172a259c33106ed28c5b296e72f17
Andrew Piper <andrew@threax.com>
2020-07-01 12:45:03 -0400

    Fixed PageNumberWidget.

--- src/PageNumberWidget.ts
+++ src/PageNumberWidget.ts
@@ -1,5 +1,5 @@
 ﻿import * as events from 'htmlrapier/src/eventdispatcher';
-import * as controller from "hr.controller";
+import * as controller from "htmlrapier/src/controller";
 
 /**
  * A toggle that is on, off or active.
